5268a31919ea54cadf839b57db2266b405b690c5
namhwui <nh35kim@gmail.com>
2021-09-17 03:26:29 -0400

    Created data generation for 8-dimensional simulation with GMM error

--- GHMR fish script.R
+++ GHMR fish script.R
@@ -12,6 +12,7 @@ data <- scale(fish[, -1])
 label <- fish$ï..Species
 y <- data[, 1]
 x <- data[, -1]
+names(data) <- colnames(fish)[-1]
 
 
 ghmr_list <- lapply(1:50, function(ii) {
@@ -23,13 +24,28 @@ gmm_list <- lapply(1:50, function(ii) {
 })
 
 rgmm_list <- lapply(1:50, function(ii) {
-  flexmix(y ~ x, data = data.frame(data), k = g, model = FLXMRrobglm(), control = list(iter = 500))
+  val <- sapply(1:6, function(g) {
+    model <- flexmix(y ~ x, data = data.frame(data), k = g, model = FLXMRrobglm(), control = list(iter = 50))
+    -stats::BIC(model)
+  })
+  G <- which.max(val)
+  flexmix(y ~ x, data = data.frame(data), k = G, model = FLXMRrobglm(), control = list(iter = 500))
 })
 
 tle_list <- lapply(1:50, function(ii) {
+  print(ii)
+  val <- sapply(1:6, function(g) {
+    model <- rmr(lr.method = "TLE", 
+                 formula   = Weight ~.,
+                 data      = data.frame(data),
+                 MaxIt     = 50, 
+                 nc        = g)
+    robmixreg_bic(model@compcoef, data)
+  })
+  G <- which.max(val)
   rmr(lr.method = "TLE", 
-      formula   = as.formula("y ~ x"),
+      formula   = Weight ~.,
       data      = data.frame(data),
-      MaxIt     = 200, 
-      nc        = g)
+      MaxIt     = 500, 
+      nc        = G)
 })
